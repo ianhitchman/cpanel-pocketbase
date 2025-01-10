25889b1354a2e73866129c396be808bd4e68f07d
Ian Hitchman <tessmarka@gmail.com>
2025-01-10 15:59:24 +1300

    Header and footer

---
+++ header.php
@@ -0,0 +1,46 @@
+<div class="pocketbase-container">
+
+<?php
+include("/usr/local/cpanel/php/cpanel.php");  // Instantiate the CPANEL object.
+$cpanel = new CPANEL();
+echo $cpanel->header();
+
+try {
+  $domain_info = $cpanel->uapi( 
+    'DomainInfo', 'domains_data',
+    array(
+        'format'    => 'hash',
+    )
+  );
+  $domain_info = $domain_info['cpanelresult'] ?? null;
+  
+  if (!$domain_info) {
+    throw new Exception("Unable to get account details.");
+  }
+    
+  $main_domain_details = $domain_info['result']['data']['main_domain'] ?? null;
+  $main_domain = $main_domain_details['domain'] ?? null;
+  $home_dir = $main_domain_details['homedir'] ?? null;
+  if (!$main_domain || !$home_dir) {
+    throw new Exception("Unable to get domain details.");
+  }
+
+  $subdomains = [];
+  $main_domain_length = strlen($main_domain);
+  $end_offset = 0 - ($main_domain_length + 1);
+  foreach ($domain_info['result']['data']['sub_domains'] as $subdomain_details) {
+    $subdomains[] = substr($subdomain_details['domain'], 0, $end_offset);
+  }
+    
+  define("PB_DOMAIN_NAME", $main_domain);
+  define("PB_URL", "https://" . PB_DOMAIN_NAME);
+  define("PB_SUBDOMAINS", $subdomains);
+  define("PB_HOME_DIR", $home_dir);
+  
+  echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"./pocketbase.css\" />";
+  
+} catch (Exception $e) {
+  echo $e->getMessage();
+}
+
+?>
--- pocketbase.live.php
+++ pocketbase.live.php
@@ -1,80 +1,38 @@
 <?php
-include("/usr/local/cpanel/php/cpanel.php");  // Instantiate the CPANEL object.
-$cpanel = new CPANEL();
-print $cpanel->header();
 
-try {
-  $domain_info = $cpanel->uapi( 
-    'DomainInfo', 'domains_data',
-    array(
-        'format'    => 'hash',
-    )
-  );
-  $domain_info = $domain_info['cpanelresult'] ?? null;
-  
-  if (!$domain_info) {
-    throw new Exception("Unable to get account details.");
-  }
-    
-  $main_domain_details = $domain_info['result']['data']['main_domain'] ?? null;
-  $main_domain = $main_domain_details['domain'] ?? null;
-  if (!$main_domain) {
-    throw new Exception("Unable to get domain details.");
-  }
+include_once("./header.php");
 
-  $subdomain = [];
-  $main_domain_length = strlen($main_domain);
-  $end_offset = 0 - ($main_domain_length + 1);
-  foreach ($domain_info['result']['data']['sub_domains'] as $subdomain_details) {
-    $subdomain[] = substr($subdomain_details['domain'], 0, $end_offset);
-  }
-    
-  define("PB_DOMAIN_NAME", $main_domain);
-  define("PB_URL", "https://" . PB_DOMAIN_NAME);
-  
-  echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"./pocketbase.css\" />";
-  
-  echo "<div class=\"pocketbase-container\">";
-  
-  echo "<section>";
-  echo "<img src=\"./pocketbase-logo.png\" alt=\"Pocketbase\" />";
-  echo "</section>";
-  
-  echo "<section>";
-  echo "To set up a new Pocketbase instance, please enter a new subdomain below.";
-  echo "</section>";
-  
+echo "<section>";
+echo "<img src=\"./pocketbase-logo.png\" alt=\"Pocketbase\" />";
+echo "</section>";
+
+echo "<section>";
+echo "To set up a new Pocketbase instance, please enter a new subdomain below.";
+echo "</section>";
+
+echo "<section>";
+echo "<div>https://</div>";
+echo "<input type=\"text\" id=\"subdomain\" name=\"subdomain\" />";
+echo "<div>." . PB_DOMAIN_NAME . "</div>";
+echo "</section>";
+
+echo "<section>";
+echo "If you leave this blank, the server will be accessible via a designated port instead<br />(e.g." . PB_URL . ":8080).";
+echo "</section>";
+
+foreach (PB_SUBDOMAINS as $subdomain) {
   echo "<section>";
   echo "<div>https://</div>";
-  echo "<input type=\"text\" id=\"subdomain\" name=\"subdomain\" />";
+  echo $subdomain;
   echo "<div>." . PB_DOMAIN_NAME . "</div>";
   echo "</section>";
-  
-  echo "<section>";
-  echo "If you leave this blank, the server will be accessible via a designated port instead<br />(e.g." . PB_URL . ":8080).";
-  echo "</section>";
-
-  foreach ($subdomain as $subdomain_name) {
-    echo "<section>";
-    echo "<div>https://</div>";
-    echo $subdomain_name;
-    echo "<div>." . PB_DOMAIN_NAME . "</div>";
-    echo "</section>";
-  }
-  
-  echo "<section>";
-  echo "<button id=\"create\">Proceed</button>";
-  echo "</section>";
-  
-  echo "<pre>";
-  print_r($domain_info);
-  echo "</pre>";
-  
-  echo "</div>";
-} catch (Exception $e) {
-  echo $e->getMessage();
 }
 
-print $cpanel->footer();                      // Add the footer.
-$cpanel->end();                               // Disconnect from cPanel - only do this once.
+echo "<section>";
+echo "<button id=\"create\">Proceed</button>";
+echo "</section>";
+
+echo "<pre>";
+print_r($domain_info);
+echo "</pre>";                          // Disconnect from cPanel - only do this once.
 ?>
